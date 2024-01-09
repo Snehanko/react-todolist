aa0252d637a7e3175c748d633181db58a0100d49
Darx <snehankobose31@gmail.com>
2024-01-09 20:43:06 +0530

    added HTML and CSS template to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
 
+import React from "react";
+
 function App() {
   return (
-    <div className="App">
-      <h1>ReactJS Todo App</h1>
+    <div className="container">
+      <div className="input-container">
+        <input type="text" placeholder="Enter Task" name="task" id="task" />
+        <button>Add Task</button>
+      </div>
+      <div className="task-list">
+        <div className="task">
+          <p>Task-1:Bring Milk</p>
+          <button>Edit Task</button>
+          <button>Delete Task</button>
+        </div>
+        <div className="task">
+          <p>Task-2:Toast Bread</p>
+          <button>Edit Task</button>
+          <button>Delete Task</button>
+        </div>
+      </div>
     </div>
   );
 }
